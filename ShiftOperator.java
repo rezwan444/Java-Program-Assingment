d27b3b994afa120b78f0e1bd0def8500e0611382
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 21:13:43 +0600

    added a shift operator program

---
+++ ShiftOperator.java
@@ -0,0 +1,26 @@
+/*
+Program no : 24
+Use of shift operator
+
+Output :      Enter a number: 10
+        Left Shift (n << 1) = 20
+        Right Shift (n >> 1) = 5
+*/
+
+import java.util.Scanner;
+
+public class ShiftOperator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Left Shift (n << 1) = ");
+        System.out.println(n << 1);
+
+        System.out.print("Right Shift (n >> 1) = ");
+        System.out.println(n >> 1);
+    }
+}

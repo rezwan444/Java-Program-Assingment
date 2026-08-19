1b256afe986a65f0824a4e62bcca90fa842ef092
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 19:49:23 +0600

    added a star triangle java program

---
+++ StarTriangle.java
@@ -0,0 +1,25 @@
+// Program no : 13
+// Star Triangle
+// output : Enter the number of rows : 10
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}

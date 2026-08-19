c7f10e8deadf7efb6586a840b5407798bb391da5
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 19:30:43 +0600

    added a sum of digits java program

---
+++ SumOfDigits.java
@@ -0,0 +1,26 @@
+// Program no : 12
+// Sum of Digit
+// Output : Enter a number : 1234
+//          Sum of Digits  : 10
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Sum of Digits = " + sum);
+    }
+}

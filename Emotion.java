bef79ecf6e007d25fe2c10ad84f38b9f3a6108b2
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 21:45:25 +0600

    print a message

---
+++ Emotion.java
@@ -0,0 +1,17 @@
+/*
+Program no : 28
+Just Print - How You Feel After Done This
+*/
+
+class Emotion {
+    public static void main(String[] args) {
+
+        System.out.println(
+            "After finishing all the programs, " +
+            "I feel a mix of emotions-some parts went right over my head, " +
+            "while I grasped a lot of it, I was confused at times, " +
+            "but ultimately, I feel good about completing it. However, " +
+            "whenever I try to explain it, everything gets jumbled up. 🙂"
+        );
+    }
+}

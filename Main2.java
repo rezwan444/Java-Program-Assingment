0cd484a8cf0e6a244431bd667d8a49d20cf7c92d
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 21:09:01 +0600

    added aonther confusion use of local and instance variable java program

--- Main2.java
+++ Main2.java
@@ -8,3 +8,30 @@ Output :      Enter age: 20
                  Marks = 85
 */
 
+import java.util.Scanner;
+
+class Student {
+    int age;   // Instance variable
+
+    void show() {
+        int marks;   // Local variable
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        age = sc.nextInt();
+
+        System.out.print("Enter marks: ");
+        marks = sc.nextInt();
+
+        System.out.println("Age = " + age);
+        System.out.println("Marks = " + marks);
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.show();
+    }
+}

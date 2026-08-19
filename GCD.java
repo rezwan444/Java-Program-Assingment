559516d3c08a443ab4f36c8ba7f5a20ffcb193a3
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 21:28:42 +0600

    added a java program - GCD

---
+++ GCD.java
@@ -0,0 +1,28 @@
+/*
+Program no : 26
+Greatest Common Divisor (GCD)
+
+Output :     Enter two numbers: 24 36
+Greatest Common Divisor (GCD) = 12
+*/
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("Greatest Common Divisor (GCD) = " + a);
+    }
+}

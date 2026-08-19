fa6b75d7a640f5436d10ce60ebdec28ce8196612
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 21:33:52 +0600

    added a java program - Least Common Multiple (LCM)

---
+++ LCM.java
@@ -0,0 +1,27 @@
+/*
+Program no : 27
+Least Common Multiple (LCM)
+
+Output :     Enter two numbers: 12 18
+  Least Common Multiple (LCM) = 36
+*/
+
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int lcm = Math.max(a, b);
+
+        while (lcm % a != 0 || lcm % b != 0)
+            lcm++;
+
+        System.out.println("Least Common Multiple (LCM) = " + lcm);
+    }
+}

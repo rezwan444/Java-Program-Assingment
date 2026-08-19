5a73225e3c598ca3e8fe1615006e9fc4eceac8b6
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 19:14:06 +0600

    added a perfect number java program

---
+++ PerfectNumber.java
@@ -0,0 +1,29 @@
+// Program no : 09
+// Perfect Number
+// output : Enter a number : 28, 6
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+    }
+}

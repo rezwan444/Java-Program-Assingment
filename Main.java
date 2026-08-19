2cdcafe8d0ebb7b1db5616fad4a367ec049234ea
Md Rezwanul Haque Nayem <rezwanulhaquenayem@gmail.com>
2026-08-19 20:58:20 +0600

    added a comfusion student 3 object program

---
+++ Main.java
@@ -0,0 +1,46 @@
+/*
+Program no : 22
+Student class with 3 object
+
+Output : Enter password: 111111
+         Enter password: 222222
+         Enter password: 515363
+         Access Granted!
+*/
+
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int age;
+
+    void show() {
+        System.out.println(name + " " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+        System.out.print("Enter 3 students name and age: ");
+
+        s1.name = sc.next();
+        s1.age = sc.nextInt();
+
+        s2.name = sc.next();
+        s2.age = sc.nextInt();
+
+        s3.name = sc.next();
+        s3.age = sc.nextInt();
+
+        s1.show();
+        s2.show();
+        s3.show();
+    }
+}
